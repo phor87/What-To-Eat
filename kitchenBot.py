7345b709f54da34f2ce23f315c93412c3b44627c
Paul Hor <32339811+paulh@users.noreply.github.com>
2018-02-07 19:20:47 -0800

    edited to return strings updated to run in discord server

--- kitchenBot.py
+++ kitchenBot.py
@@ -1,4 +1,5 @@
 import pickle
+import discord
 
 recipes_dict = pickle.load(open("recipes.pickle", "rb"))
 ingredients_list = open('ingredients.txt').read().splitlines()
@@ -20,54 +21,55 @@ def have(ingredient=None):
             if set(ingredient).issubset(ingredients_list):
                 a_list.append(recipe)
         if not a_list:
-            print('We have nothing.')
+            return 'We have nothing.'
         else:
-            print('We have', a_list)
+
+            return 'We have ' + ', '.join(a_list)
     else:
         for item in recipes_dict:
             if ingredient in recipes_dict[item]:
                 a_list.append(item)
         if not a_list:
-            print('Nothing has', ingredient, 'in it.')
+            return 'Nothing has ' + str(ingredient) + ' in it.'
         else:
-            print('These have', ingredient, 'in it.', a_list)
+            return 'These have ' + str(ingredient) + ' in it: ' + ', '.join(a_list)
 
 
 def add(ingredient):
     """ add an ingredient to the list """
     ingredient = ingredient.lower()
     if ingredient == '':
-        print('Nothing to add.')
+        return 'Nothing to add.'
     elif ingredient not in ingredients_list:
         ingredients_list.append(ingredient)
-        print(ingredient, 'added to ingredients list.')
+        return str(ingredient) + ' added to ingredients list.'
         update_list()
     elif ingredient in ingredients_list:
-        print(ingredient, 'is already in ingredients list.')
+        return str(ingredient) + ' is already on the list.'
 
 
 def remove(ingredient):
     """ remove an ingredient from the list """
     if ingredient == '':
-        print('Nothing to add.')
+        return 'Nothing to add.'
     if ingredient in ingredients_list:
         ingredients_list.remove(ingredient)
-        print(ingredient, 'was removed from ingredients list.')
+        return str(ingredient) + ' was removed from the list.'
         update_list()
     elif ingredient not in ingredients_list:
-        print(ingredient, 'is not in the ingredients list.')
+        return str(ingredient) + ' is not in the list.'
 
 
 def eat(something):
     """ check if something is available or prints a list of ingredients needed """
     if set(recipes_dict[something]).issubset(ingredients_list):
-        print('We can eat', something)
+        return 'We can eat ' + str(something)
     else:  # prints what ingredients are needed
         a_list = []
         for ingredient in recipes_dict[something]:
             if ingredient not in ingredients_list:
                 a_list.append(ingredient)
-        print('For', something, 'we need', a_list)
+        return 'For ' + str(something) + ', we need' + ', '.join(a_list)
 
 
 def shop():
@@ -78,7 +80,7 @@ def shop():
             for items in ingredient:
                 if items not in ingredients_list and items not in shopping_list:
                     shopping_list.append(items)
-    print('Shopping list:', shopping_list)
+    return 'Shopping list: ' + ', '.join(shopping_list)
 
 
 def save():
@@ -90,11 +92,50 @@ def save():
 
 def list_recipes():
     """ list recipes from dictionary """
-    for recipe in recipes_dict:
-        print(recipe)
+    return ', '.join(str(recipe) for recipe in recipes_dict)
 
 
 def list_ingredients():
     """ list ingredients from list """
-    for items in ingredients_list:
-        print(items)
+    return ', '.join(str(items) for items in ingredients_list)
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('.have'):
+        await client.send_message(message.channel, have())
+
+    if message.content.startswith('.recipes'):
+        await client.send_message(message.channel, list_recipes())
+
+    if message.content.startswith('.ingredients'):
+        await client.send_message(message.channel, list_ingredients())
+
+    if message.content.startswith('.shop'):
+        await client.send_message(message.channel, shop())
+
+    if message.content.startswith('.add'):
+        item = await client.wait_for_message(timeout=20.0, author=message.author)
+        await client.send_message(message.channel, add(str(item.content)))
+
+    if message.content.startswith('.remove'):
+        item = await client.wait_for_message(timeout=20.0, author=message.author)
+        await client.send_message(message.channel, remove(str(item.content)))
+
+    if message.content.startswith('.eat'):
+        item = await client.wait_for_message(timeout=20.0, author=message.author)
+        await client.send_message(message.channel, eat(str(item.content)))
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run('token')
